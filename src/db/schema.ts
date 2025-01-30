37a4f0ebdd383eea1aafba3279b56885c803a6a7
DomineersXII <channen1titan@gmail.com>
2025-01-30 17:26:57 -0500

    fixing node modules

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core"
+import { sqliteTable, text } from "drizzle-orm/sqlite-core"
 
 export const usersTable = sqliteTable("users_table", {
     username: text().notNull().unique(),
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,16 @@
-import "dotenv/config"
-import { drizzle } from "drizzle-orm/libsql"
-import { usersTable } from "./db/schema"
-import bcrypt from "bcrypt"
-import { eq } from "drizzle-orm"
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
 
-function hashPassword(password: string) {
-    const HASH_TIMES = 5
+const app = new Hono()
 
-    return bcrypt.hashSync(password, HASH_TIMES)
-}
-
-
-const db = drizzle({
-    connection: {
-        url: process.env.TURSO_DATABASE_URL!,
-        authToken: process.env.TURSO_AUTH_TOKEN!
-    }
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
 })
 
-/* 
-    Check if the inputted username is already in use. If not, save the inputted username and password (hashed) to the database.
-*/
-async function signUp(username: string, password: string): Promise<boolean> {
-    const foundUser = await db.select().from(usersTable).where(eq(usersTable.username, username))
+const port = 3000
+console.log(`Server is running on http://localhost:${port}`)
 
-    if (foundUser.length !== 0) {
-        return false
-    }
-    
-    const hashedPassword = hashPassword(password)
-    const user: typeof usersTable.$inferInsert = {
-        username: username,
-        password: hashedPassword
-    }
-
-    await db.insert(usersTable).values(user)
-    
-    return true
-}
-
-/* 
-    Check if the inputted username is stored in the database. If true then attempt to sign in.
-*/
-async function signIn(username: string, password: string) {
-    const foundUser = await db.select().from(usersTable).where(eq(usersTable.username, username))
-
-    if (foundUser.length === 0) {
-        throw new Error("User doesn't exist")
-    }
-
-    return await bcrypt.compare(password, foundUser[0].password);
-}
+serve({
+  fetch: app.fetch,
+  port
+})
